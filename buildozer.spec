0eab315d1f9f8ae7b7fa43e93400636687c42c82
Vyshak <vyshaknsc02@gmail.com>
2023-05-07 23:35:34 +0530

    Initial commit

---
+++ buildozer.spec
@@ -0,0 +1,94 @@
+[app]
+
+# (str) Title of your application
+title = PdfSearch
+
+# (str) Package name
+package.name = pdfsearch
+
+# (str) Package domain (needed for android/ios packaging)
+package.domain = org.example
+
+# (str) Source code where the main.py live
+source.dir = .
+
+# (str) The version of your application
+version = 1.0
+
+# (list) Application requirements
+requirements = kivy, android.permissions
+
+# (str) Custom source folders for requirements
+# Sets custom folder for kivy requirements because the defaults are too big
+source.include_exts = py,png,jpg,kv,atlas
+source.kivy = kivy
+
+# (list) Permissions
+android.permissions = WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE
+
+# (list) Services
+android.services = 
+
+# (str) Android API to use
+android.api = 29
+
+# (bool) Indicate whether the application should be fullscreen or not
+fullscreen = 1
+
+# (list) List of service to declare
+services = 
+
+# (str) The orientation to start with
+orientation = portrait
+
+# (list) Permissions
+android.permissions = WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE
+
+# (list) List of features to declare
+android.features = android.hardware.usb.host
+
+# (list) List of Java classes to add to the android project
+android.add_jars = 
+
+# (list) List of Java *.jar files to add to the android project (can be absolute or relative to the spec file)
+android.add_jars_app = 
+
+# (list) Android AAR archives to add (currently works only with sdl2_gradle)
+#android.add_aars = 
+
+# (list) Gradle dependencies to add (currently works only with sdl2_gradle)
+#android.gradle_dependencies = 
+
+# (list) application meta-data to set (key=value format)
+#android.meta_data = 
+
+# (list) Activity attributes to declare
+#android.manifest_additions = 
+
+# (list) xml files to include
+#android.add_xml = 
+
+# (list) Java files to add (can be java or a referenced jar files)
+#android.add_src = 
+
+# (str) Path to a custom toolchain to be used
+#android.toolchain = /opt/android-sdk/
+
+# (str) Path to a custom ndk to be used (if empty, it will be automatically downloaded.)
+#android.ndk_path = 
+
+# (bool) Use a black overlay to hide the contents of the screen. This is useful for
+# taking screenshots of the application without having to modify any of the UI.
+#android.screenshot = False
+
+# (str) Supported orientation (one of landscape, sensorLandscape, portrait or all)
+#android.orientation = portrait
+
+# (bool) Indicate whether the application should be resizable (Android only)
+#resizable = 0
+
+# (str) Android logcat filters to use
+#android.logcat_filters = *:S python:D
+
+# (str) Android additional libraries to copy into libs/armeabi
+#android.add_libs_armeabi = libs/android/*.so
---
+++ main.py
@@ -0,0 +1,81 @@
+import kivy,os
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.filechooser import FileChooserListView
+from android.permissions import request_permissions, Permission
+
+request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE])
+
+root_dir = os.getcwd()
+
+class RootDirScreen(Screen):
+    def set_root_dir(self):
+        self.parent.parent.root_dir = self.ids.file_chooser.path
+        self.parent.dismiss()
+
+
+class PdfSearch(BoxLayout):
+    def __init__(self, **kwargs):
+        super(PdfSearch, self).__init__(**kwargs)
+        self.input = self.ids.input
+        self.results = []
+
+    def select_root_dir(self, event):
+        # Open a file browser to select the root directory
+        dir_content = BoxLayout(orientation='vertical')
+        file_chooser = FileChooserListView()
+        dir_content.add_widget(file_chooser)
+
+        popup = Popup(title='Select Root Directory',
+                      content=dir_content,
+                      size_hint=(0.9, 0.9),
+                      auto_dismiss=False)
+
+        def set_root_dir(event):
+            self.root_dir = file_chooser.path
+            popup.dismiss()
+
+        set_dir_button = Button(text='Set Root Directory',
+                                size_hint_y=None,
+                                height='50dp')
+        set_dir_button.bind(on_press=set_root_dir)
+        dir_content.add_widget(set_dir_button)
+
+        popup.open()
+
+
+    def search(self, event):
+        self.found_list = []
+        self.ids.result_box.clear_widgets()
+        ext = self.ids.extension.text
+        extension = '.' + self.ids.extension.text if ext else '.txt'
+        key = self.input.text
+        for dir, sub_dir, file_lst in os.walk(root_dir):
+            for file in file_lst:
+                if file.endswith(extension):
+                    with open(file,'r+') as item:
+                        data = item.read()
+                        if key in data:
+                            self.found_list.append(file)
+        self.add_results()
+  
+    def add_results(self):
+        for i in self.found_list:
+            button_exists = False
+            for child in self.ids.result_box.children:
+                if isinstance(child, Button) and child.text == i:
+                    button_exists = True
+                    break
+            if not button_exists:
+                lbl = Button(text=i, background_color=(0.2, 0.2, 0.2))
+                self.ids.result_box.add_widget(lbl)
+
+class PdfSearchApp(App):
+    def build(self):
+        return PdfSearch()
+
+if __name__ == '__main__':
+    PdfSearchApp().run()
